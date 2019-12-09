c360ec15ea4f2c526016603dbe0c53284f55a6f4
Tóth Máté <tothmate911@gmail.com>
2019-12-09 14:10:53 +0100

    data handler get question by id

--- data_handler.py
+++ data_handler.py
@@ -4,6 +4,7 @@ ANSWER_FILE_PATH = 'sample_data/answer.csv'
 QUESTION_FILE_PATH = 'sample_data/question.csv'
 DATA_HEADER =['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
 
+
 def get_all_questions():
     all_questions = get_list_of_dictionaries_from_csv(QUESTION_FILE_PATH)
     return  all_questions
@@ -19,3 +20,10 @@ def get_list_of_dictionaries_from_csv(path):
         reader = csv.DictReader(csvfile, fieldnames=DATA_HEADER)
         all_data = [data for data in reader]
     return all_data
+
+
+def get_question_by_id(id):
+    all_questions = get_all_questions()
+    for question in all_questions:
+        if question['id'] == id:
+            return question
