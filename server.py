1f994117a25f6707ba3e8a653cf1fd2e682b97cf
Tóth Máté <tothmate911@gmail.com>
2019-12-10 13:34:47 +0100

    answer update

--- server.py
+++ server.py
@@ -1,27 +1,32 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import data_handler
 app = Flask(__name__)
 
-@app.route('/')
-def route_index():
 
-    return render_template('lists.html')
+@app.route('/')
+@app.route('/list')
+def route_index():
+    all_questions = data_handler.get_all_questions()
+    return render_template('lists.html', all_questions=all_questions)
 
 @app.route('/add_question', methods=['GET', 'POST'])
-def route_edit():
+def add_question():
     if request.method == 'POST':
         question={}
+        for titles in data_handler.QUESTION_DATA_HEADER[1:]:
 
-        for titles in data_handler.DATA_HEADER[1:]:
             question.update({titles : request.form[titles]})
         data_handler.add_question(question)
         return redirect('/'),
 
     return render_template('add_question.html')
 
-@app.route('/lists')
-def show_questions():
-    return render_template('lists.html', data_handler.get_all_questions())
+@app.route('/show_question/<question_id>', methods=['GET', 'POST'])
+def show_question(question_id):
+    question = data_handler.get_question_by_id(question_id)
+    answers_for_question = data_handler.get_all_answers_for_a_question(question_id)
+    return render_template('answer.html', question=question, answers_for_question=answers_for_question)
+
 
 if __name__ == "__main__":
     app.run(
