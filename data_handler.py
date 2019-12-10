a6e8f260e8638a7f7af4c18730b35049d3db57be
Pivi14 <pivarcsiadam@gmail.com>
2019-12-10 10:50:51 +0100

    "test"

--- data_handler.py
+++ data_handler.py
@@ -7,12 +7,12 @@ DATA_HEADER =['id', 'submission_time', 'view_number', 'vote_number', 'title', 'm
 
 def get_all_questions():
     all_questions = get_list_of_dictionaries_from_csv(QUESTION_FILE_PATH)
-    return  all_questions
+    return all_questions
 
 
 def get_all_answers():
     all_answers = get_list_of_dictionaries_from_csv(ANSWER_FILE_PATH)
-    return  all_answers
+    return all_answers
 
 
 def get_list_of_dictionaries_from_csv(path):
@@ -35,4 +35,24 @@ def next_id_generator(path):
         next_id = int(data[-1]['id']) + 1
     except ValueError:
         next_id = 0
-    return  next_id
+    return next_id
+
+def add_question(question):
+    question['id'] = next_id_generator(question)
+    write_the_file(QUESTION_FILE_PATH, question, append=True)
+
+def write_the_file(file_name, write_elements, append=True):
+    existing_data = get_all_questions()
+    with open(file_name, 'w', newline='', encoding='utf-8') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=DATA_HEADER)
+        writer.writeheader()
+
+        for row in existing_data:
+            if not append:
+                if row['id'] == write_elements['id']:
+                    row = write_elements
+
+            writer.writerow(row)
+
+        if append:
+            writer.writerow(write_elements)
--- server.py
+++ server.py
@@ -1,27 +1,27 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 import data_handler
 app = Flask(__name__)
 
 @app.route('/')
-def route_index():
-
-    return render_template('index.html')
+@app.route('/lists')
+def route_lists():
+    questions = data_handler.get_all_questions()
+    return render_template('lists.html', question=questions)
 
 @app.route('/add_question', methods=['GET', 'POST'])
 def route_edit():
     if request.method == 'POST':
-        question={}
-
+        question = {}
         for titles in data_handler.DATA_HEADER[1:]:
-            question.update({titles : request.form[titles]})
+            question.update({titles: request.form.get[titles]})
         data_handler.add_question(question)
-        return redirect('/'),
+        return redirect('/lists'),
 
-    return render_template('add_question.html.html')
-
-@app.route('/lists')
-def show_questions():
-    return render_template('lists.html', data_handler.get_all_questions())
+    return render_template('add_question.html',
+                           comment_name='Add new question',
+                           form_url=url_for('new_question_add'),
+                           comment_title='Question title',
+                           comment_message='Question message')
 
 if __name__ == "__main__":
     app.run(
