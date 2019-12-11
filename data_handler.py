f54c3b9f8253be8f5a9a9ae2ecba1b5229ab2641
pivarcsi <pivarcsiadam@gmail.com>
2019-12-11 10:39:11 +0100

    "new master"

--- data_handler.py
+++ data_handler.py
@@ -1,64 +1,101 @@
 import csv
+from datetime import datetime
+import time
 
 ANSWER_FILE_PATH = 'sample_data/answer.csv'
 QUESTION_FILE_PATH = 'sample_data/question.csv'
-ANSWER_DATA_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-QUESTION_DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+DATA_HEADER =['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+ANSWER_HEADER =['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 
 
-def get_all_questions():
-    all_questions = get_list_of_dictionaries_from_csv(QUESTION_FILE_PATH, QUESTION_DATA_HEADER)
+def get_all_questions(time=False):
+    all_questions = get_list_of_dictionaries_from_csv(QUESTION_FILE_PATH, DATA_HEADER, time)
     return all_questions
 
 
-def get_all_answers():
-    all_answers = get_list_of_dictionaries_from_csv(ANSWER_FILE_PATH, ANSWER_DATA_HEADER)
+def get_all_answers(time=False):
+    all_answers = get_list_of_dictionaries_from_csv(ANSWER_FILE_PATH, ANSWER_HEADER, time)
     return all_answers
 
 
-def get_list_of_dictionaries_from_csv(path, actual_data_header):
-    with open(path) as csvfile:
-        reader = csv.DictReader(csvfile, fieldnames=actual_data_header)
-        all_data = [data for data in reader]
+def get_list_of_dictionaries_from_csv(path, header, time=False):
+    with open(path, encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file, fieldnames=header)
+        all_data = []
+        if not time:
+            for data in reader:
+                if data['id'] != 'id':
+                    all_data.append(data)
+        else:
+            for data in reader:
+                if data['id'] != 'id':
+                    real_time = datetime.fromtimestamp(int(data['submission_time']))
+                    data['submission_time'] = real_time
+                    all_data.append(data)
     return all_data
 
 
-def get_question_by_id(question_id):
+def get_question_by_id(id):
     all_questions = get_all_questions()
     for question in all_questions:
-        if question['id'] == question_id:
+        if question['id'] == id:
             return question
 
 
-def get_all_answers_for_a_question(question_id):
-    all_answers = get_all_answers()
-    answers_for_a_question = [answer for answer in all_answers if answer['question_id'] == question_id]
-    return answers_for_a_question
-
-
-def next_id_generator(path):
-    actual_data_header = []
-    if path == QUESTION_FILE_PATH:
-        actual_data_header = QUESTION_DATA_HEADER
-    elif path == ANSWER_FILE_PATH:
-        actual_data_header = ANSWER_DATA_HEADER
-    data = get_list_of_dictionaries_from_csv(path, actual_data_header)
+def next_id_generator(path, header):
+    data = get_list_of_dictionaries_from_csv(path, header)
     try:
         next_id = int(data[-1]['id']) + 1
     except ValueError:
         next_id = 0
     return next_id
 
+def add_question(question):
+    question['id'] = next_id_generator(QUESTION_FILE_PATH, DATA_HEADER)
+    question['submission_time'] = date_time_in_timestamp()
+    write_the_file(QUESTION_FILE_PATH, question, DATA_HEADER, append=True)
 
-def add_question(quiestion_to_add):
-    with open(QUESTION_FILE_PATH, 'a') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=QUESTION_DATA_HEADER)
-        writer.writerow(quiestion_to_add)
-    pass
+def add_answer(answer, question_id):
+    answer['id'] = next_id_generator(ANSWER_FILE_PATH, DATA_HEADER)
+    answer['submission_time'] = date_time_in_timestamp()
+    answer['question_id'] = question_id
+    write_the_file(ANSWER_FILE_PATH, answer, ANSWER_HEADER, append=True)
 
+def write_the_file(file_name, write_elements, header, append=True):
+    if header == ANSWER_HEADER:
+        existing_data = get_all_answers()
+    else:
+        existing_data = get_all_questions()
+    with open(file_name, 'w', encoding='utf-8') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=header)
+        writer.writeheader()
 
-def add_answer(answer_to_add):
-    with open(ANSWER_FILE_PATH, 'a') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=ANSWER_DATA_HEADER)
-        writer.writerow(answer_to_add)
-    pass
+        for row in existing_data:
+            if not append:
+                if row['id'] == write_elements['id']:
+                    row = write_elements
+
+            writer.writerow(row)
+
+        if append:
+            writer.writerow(write_elements)
+
+def one_question(question_id, time=False):
+    all_question = get_all_questions(time)
+    for question in all_question:
+        if question['id'] == question_id:
+            return question
+
+def all_answer_for_one_question(question_id):
+    answers = []
+    all_answer = get_all_answers(time=True)
+    for answer in all_answer:
+        if answer['question_id'] == str(question_id):
+            answers.append(answer)
+    return answers
+
+def date_time_in_timestamp():
+    return int(time.time())
+
+def real_date_time(timestamp):
+    return datetime.fromtimestamp(timestamp)
--- server.py
+++ server.py
@@ -2,31 +2,58 @@ from flask import Flask, render_template, redirect, request, url_for
 import data_handler
 app = Flask(__name__)
 
-
 @app.route('/')
-@app.route('/list')
-def route_index():
-    all_questions = data_handler.get_all_questions()
-    return render_template('lists.html', all_questions=all_questions)
-
+@app.route('/lists')
+def route_lists():
+    questions = data_handler.get_all_questions(time=True)
+    questions.reverse()
+    return render_template("lists.html", question=questions)
 
 @app.route('/add_question', methods=['GET', 'POST'])
-def add_question():
+def route_new_question():
     if request.method == 'POST':
-        question = {}
-        for title in data_handler.QUESTION_DATA_HEADER[1:]:
-            question.update({title: request.form[title]})
-        data_handler.add_question(question)
-        return redirect('/')
+        comment = {'title': request.form.get('title'),
+                   'message': request.form.get('message'),
+                   'submission_time': request.form.get('submission_time'),
+                   'view_number': request.form.get('view_number'),
+                   'vote_number': request.form.get('vote_number'),
+                   'image': request.form.get('image')}
+        data_handler.add_question(comment)
+        return redirect('/lists')
 
-    return render_template('add_question.html')
+    return render_template("add_question.html",
+                           comment_name='Add new question',
+                           form_url=url_for('route_new_question'),
+                           comment_title='Question title',
+                           comment_message='Question message',
+                           type='question')
 
+@app.route('/question/<question_id>')
+def route_question(question_id):
+    question = data_handler.one_question(question_id, time=True)
+    answer = data_handler.all_answer_for_one_question(question_id)
+    return render_template("answer.html",
+                           question=question,
+                           answer=answer)
 
-@app.route('/question/<question_id>', methods=['GET', 'POST'])
-def show_question(question_id):
-    question = data_handler.get_question_by_id(question_id)
-    answers_for_question = data_handler.get_all_answers_for_a_question(question_id)
-    return render_template('show_question.html', question=question, answers_for_question=answers_for_question)
+@app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
+def route_new_answer(question_id):
+    if request.method == 'POST':
+        comment = {'message': request.form.get('message'),
+                   'submission_time': request.form.get('submission_time'),
+                   'vote_number': request.form.get('vote_number'),
+                   'image': request.form.get('image'),
+                   'question_id': request.form.get('question_id')}
+        page_return = f'/question/{question_id}'
+        data_handler.add_answer(comment, question_id)
+        return redirect(page_return)
+
+    return render_template("add_question.html",
+                           comment_name='Add new answer',
+                           form_url=url_for('route_new_answer', question_id=question_id),
+                           comment_message='Answer message',
+                           question_id=question_id,
+                           timestamp=data_handler.date_time_in_timestamp())
 
 
 if __name__ == "__main__":
