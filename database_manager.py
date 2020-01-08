598984bb746c126feadbdf459f5930513d3bf8f7
pivarcsi <pivarcsiadam@gmail.com>
2020-01-08 08:47:28 +0100

    sql system

--- database_manager.py
+++ database_manager.py
@@ -24,8 +24,13 @@ def add_question(cursor, new_question):
                     't': new_question['title'],
                     'm': new_question['message'],
                     'i': new_question['image']})
-    pass
 
+@database_common.connection_handler
+def add_answer(cursor, new_answer):
+    cursor.execute(f"""
+                INSERT INTO answer (submission_time, vote_number, question_id, message, image)
+                VALUES ({new_answer['submission_time']}, {new_answer['vote_number']}, {new_answer['question_id']}, {new_answer['message']}, {new_answer['image']})     
+    """)
 
 @database_common.connection_handler
 def get_question_by_id(cursor, question_id):
@@ -34,8 +39,41 @@ def get_question_by_id(cursor, question_id):
                     WHERE id = {question_id};
                     """)
     question = cursor.fetchall()
-    print(question)
     return question
+
+@database_common.connection_handler
+def get_answer_by_id(cursor, answer_id):
+    cursor.execute(f"""
+                    SELECT * FROM answer
+                    WHERE id = {answer_id};
+                    """)
+    answer = cursor.fetchall()
+    return answer
+
+@database_common.connection_handler
+def get_all_answer_by_question_id(cursor, question_id):
+    cursor.execute(f"""
+                    SELECT * FROM answer
+                    WHERE question_id={question_id}
+                    """)
+    answers = cursor.fetchall()
+    return answers
+
+@database_common.connection_handler
+def delete_question(cursor, question_id):
+    cursor.execute(f"""
+                    DELETE FROM question
+                    WHERE id = {question_id};
+                    DELETE FROM answer
+                    WHERE question_id = {question_id}
+""")
+
+@database_common.connection_handler
+def delete_answer(cursor, answer_id):
+    cursor.execute(f"""
+                    DELETE FROM answer
+                    WHERE id = {answer_id}
+""")
 
 
 @database_common.connection_handler
@@ -66,6 +104,34 @@ def write_new_comment(cursor,to_write_dict):
     columns = to_write_dict.keys()
     values = [to_write_dict[column] for column in columns]
 
-    insert_statement = 'insert into comment (%s) values %s'
+    insert_statement = '''insert into comment (%s) values (%s)'''
 
     cursor.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
+
+@database_common.connection_handler
+def vote(cursor,id, type, positive):
+    if positive:
+        vote = 1
+    else:
+        vote = -1
+    cursor.execute(f"""
+                    UPDATE {type}
+                    SET vote_number = vote_number + {vote}
+                    WHERE id = {id}
+                    """)
+
+@database_common.connection_handler
+def update_question(cursor, question, id):
+    cursor.execute(f"""
+                    UPDATE question
+                    SET title = '{question['title']}', message = '{question['message']}'
+                    WHERE id = {id}
+""")
+
+@database_common.connection_handler
+def update_answer(cursor, answer, id):
+    cursor.execute(f"""
+                    UPDATE answer
+                    SET message = '{answer['message']}'
+                    WHERE id = {id}
+""")
